816cf7b39d11f82d5b17f00ad4ddbf9127809713
peter jackson <peterndigara@gmail.com>
2022-08-24 03:30:47 +0300

    done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
@@ -10,7 +11,22 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+// const titleCased = () => {
+//   return tutorials.map((line) => {
+//     const tokens = line.split(" ");
+//     const capitalizedTokens = tokens.map(
+//       (token) => token.charAt(0).toUpperCase() + token.slice(1)
+//     );
+//     const response = capitalizedTokens.join(" ");
+//     return response;
+//   })
+// }
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((line) => {
+    const words = line.split(" ");
+    const  caps = words.map( (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    const capitalized = caps.join(" ");
+    return capitalized;
+  })
 }
